f633fde3ea86e651f59229754d4f13df1f9fed48
manirul <manirul26@gmail.com>
2024-03-08 22:38:53 +0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import ReactPDF from '@react-pdf/renderer';
+import jsPDF from 'jspdf'; //npm install jspdf
+
+import { PDFViewer } from '@react-pdf/renderer';
+import ReportTemplate from "./file/ReportTemplate";
+//import MyDocument from "./file/MyDocument";
 
 function App() {
+  const reportTemplateRef = useRef(null);
+  
+  const download = () => {
+    const doc = new jsPDF({
+      format: 'a4',
+      unit: 'px',
+    });
+
+    // Adding the fonts.
+    doc.setFont('Inter-Regular', 'normal');
+
+    doc.html(reportTemplateRef.current, {
+      async callback(doc) {
+        await doc.save('document');
+      },
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100%' }}>
+      <h3>Invoice No</h3>
+      <button onClick={download}>Download</button>
+
+      <div ref={reportTemplateRef}>
+        <ReportTemplate />
+      </div>
     </div>
+    /*  <PDFViewer style={{ width: '100%', height: 700 }}>
+    https://www.youtube.com/watch?v=Dc-HUp-iI4E  JS PDF 
+      <MyDocument />
+     </PDFViewer> */
   );
 }
 
 export default App;
+
+
